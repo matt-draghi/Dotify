28556f2b6996c46c0f95db09a05d07ea03eb4b55
Matt Draghi <mtd5264@gmail.com>
2022-03-08 14:49:50 -0500

    small change to imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import './styles/App.css'
-import {BrowserRouter, BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.render(
   <Router>
@@ -11,7 +11,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
