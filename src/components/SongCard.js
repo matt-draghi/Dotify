bccb31efba0d3735109c064749969eb434f5c391
Matt Draghi <mtd5264@gmail.com>
2022-03-10 11:03:25 -0500

    Youtube video plays on album click; can close or minimize

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,18 +1,56 @@
+import { useState } from "react"
+
 function SongCard({song}){
+
+    const [videoId, setVideoId] = useState("")
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    const handleSongPlay = () => {
+        setModalIsOpen(true)
+        extractVideoID(song.youtube_link)
+        
+    }
+
+    const closeModal = () =>{
+        setModalIsOpen(false)
+        setVideoId(null)
+    }
+
+    const minimizeModal = () =>{
+        setModalIsOpen(false)
+    }
+
+    const extractVideoID = (url) => {
+        const videoCode = url.split("v=")[1].split("&")[0];
+        setVideoId(videoCode)
+      }
 
     return(
         <div className="playlist-song-container">
             
-                <div className="song-card">
-                    <a href={song.youtube_link} target="_blank" rel="noreferrer" className='song-link'>
-                        <img alt={song.title} src={song.album.album_cover} width="120" height="120"/>
-                        <div className="song-details">
-                            <h4>{song.title} </h4>
-                            <p>{song.album.artist.name}</p>
-                        </div>
-                    </a>
+             <div className="song-card">
+                <img alt={song.title} src={song.album.album_cover} width="120" height="120" onClick={handleSongPlay}/>
+                <div className="song-details">
+                    <h4>{song.title} </h4>
+                    <p>{song.album.artist.name}</p>
                 </div>
-            
+            </div>
+
+            <div className={modalIsOpen ? 'iframe-modal-active' : 'modal'} id='modal'>
+                <div className='modal-header'>
+                    <div className='title'>
+                        {song.title}
+                    </div>
+                    <button onClick={minimizeModal}className='close-button'>🗕</button>
+                    <button onClick={closeModal}className='close-button'>x</button>
+                </div>
+                <div className='modal-body'>
+                <iframe src={`https://www.youtube.com/embed/${videoId}`}></iframe>
+                {console.log("url:", song.youtube_link)}
+                {console.log("video link", `https://www.youtube.com/embed/${videoId}`)}
+                </div>
+            </div>
+            <div id={modalIsOpen ? 'overlay-active' : 'overlay'}> </div>
         </div>
     )
 }
